bc686b468fe760dcb5ad524c26529fe9c8cebbcf
skimaksss1 <skimaksss@gmail.com>
2023-11-12 08:07:26 +0600

    commit

--- script.js
+++ script.js
@@ -23,19 +23,19 @@ $("body").on('click', '[href*="#"]', function(e){
   });
 
 
-  function onEntry(entry) {
-	entry.forEach(change => {
-		if (change.isIntersecting) {
-		change.target.classList.add('element-show');
-		}
-	});
-	}
+//   function onEntry(entry) {
+// 	entry.forEach(change => {
+// 		if (change.isIntersecting) {
+// 		change.target.classList.add('element-show');
+// 		}
+// 	});
+// 	}
 
-	let options = {
-	threshold: [0.25] };
-	let observer = new IntersectionObserver(onEntry, options);
-	let elements = document.querySelectorAll('.element-animation');
+// 	let options = {
+// 	threshold: [0.25] };
+// 	let observer = new IntersectionObserver(onEntry, options);
+// 	let elements = document.querySelectorAll('.element-animation');
 
-	for (let elm of elements) {
-	observer.observe(elm);
-	}
+// 	for (let elm of elements) {
+// 	observer.observe(elm);
+// 	}
