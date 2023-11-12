c240a18b2d8423edf5e171ff5cdc6071523eb102
skimaksss1 <skimaksss@gmail.com>
2023-11-12 07:49:25 +0600

    commit

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 				$(this).find('.open_close-btn').toggleClass('filter_invert');
 				$('.left_content').toggleClass('filter_invert');
 		});
+		
 	}))
 });
 
@@ -20,3 +21,21 @@ $("body").on('click', '[href*="#"]', function(e){
 	$('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
 	e.preventDefault();
   });
+
+
+  function onEntry(entry) {
+	entry.forEach(change => {
+		if (change.isIntersecting) {
+		change.target.classList.add('element-show');
+		}
+	});
+	}
+
+	let options = {
+	threshold: [0.25] };
+	let observer = new IntersectionObserver(onEntry, options);
+	let elements = document.querySelectorAll('.element-animation');
+
+	for (let elm of elements) {
+	observer.observe(elm);
+	}
